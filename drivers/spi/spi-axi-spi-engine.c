2194d36c724dd9fdbe9fd4fbe391215e1056cd96
Drew Fustini <dfustini@baylibre.com>
2023-10-23 07:56:03 -0700

    [DO NOT MERGE] spi: spi-engine: Add offload support
    Changes that resulted from copying drivers/spi/spi-axi-spi-engine.c
    from adi/master (v5.15) to mainline. The purpose is to compile test.

--- drivers/spi/spi-axi-spi-engine.c
+++ drivers/spi/spi-axi-spi-engine.c
@@ -12,6 +12,7 @@
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/spi/spi.h>
+#include <linux/timer.h>
 
 #define SPI_ENGINE_VERSION_MAJOR(x)	((x >> 16) & 0xff)
 #define SPI_ENGINE_VERSION_MINOR(x)	((x >> 8) & 0xff)
@@ -36,10 +37,19 @@
 #define SPI_ENGINE_REG_SDI_DATA_FIFO		0xe8
 #define SPI_ENGINE_REG_SDI_DATA_FIFO_PEEK	0xec
 
+#define SPI_ENGINE_REG_OFFLOAD_CTRL(x)		(0x100 + (0x20 * x))
+#define SPI_ENGINE_REG_OFFLOAD_STATUS(x)	(0x104 + (0x20 * x))
+#define SPI_ENGINE_REG_OFFLOAD_RESET(x)		(0x108 + (0x20 * x))
+#define SPI_ENGINE_REG_OFFLOAD_CMD_MEM(x)	(0x110 + (0x20 * x))
+#define SPI_ENGINE_REG_OFFLOAD_SDO_MEM(x)	(0x114 + (0x20 * x))
+
 #define SPI_ENGINE_INT_CMD_ALMOST_EMPTY		BIT(0)
 #define SPI_ENGINE_INT_SDO_ALMOST_EMPTY		BIT(1)
 #define SPI_ENGINE_INT_SDI_ALMOST_FULL		BIT(2)
 #define SPI_ENGINE_INT_SYNC			BIT(3)
+
+#define SPI_ENGINE_OFFLOAD_CTRL_ENABLE		BIT(0)
+#define SPI_ENGINE_OFFLOAD_STATUS_ENABLED	BIT(0)
 
 #define SPI_ENGINE_CONFIG_CPHA			BIT(0)
 #define SPI_ENGINE_CONFIG_CPOL			BIT(1)
@@ -52,6 +62,7 @@
 
 #define SPI_ENGINE_CMD_REG_CLK_DIV		0x0
 #define SPI_ENGINE_CMD_REG_CONFIG		0x1
+#define SPI_ENGINE_CMD_REG_WORD_LENGTH		0x2
 
 #define SPI_ENGINE_MISC_SYNC			0x0
 #define SPI_ENGINE_MISC_SLEEP			0x1
@@ -82,6 +93,8 @@ struct spi_engine {
 	struct clk *clk;
 	struct clk *ref_clk;
 
+	struct spi_master *master;
+
 	spinlock_t lock;
 
 	void __iomem *base;
@@ -103,6 +116,9 @@ struct spi_engine {
 	unsigned int completed_id;
 
 	unsigned int int_enable;
+
+	struct timer_list watchdog_timer;
+	unsigned int word_length;
 };
 
 static void spi_engine_program_add_cmd(struct spi_engine_program *p,
@@ -131,15 +147,45 @@ static unsigned int spi_engine_get_clk_div(struct spi_engine *spi_engine,
 	struct spi_device *spi, struct spi_transfer *xfer)
 {
 	unsigned int clk_div;
+	unsigned int speed;
+
+	if (xfer->speed_hz)
+		speed = xfer->speed_hz;
+	else
+		speed = spi->max_speed_hz;
 
 	clk_div = DIV_ROUND_UP(clk_get_rate(spi_engine->ref_clk),
-		xfer->speed_hz * 2);
+		speed * 2);
 	if (clk_div > 255)
 		clk_div = 255;
 	else if (clk_div > 0)
 		clk_div -= 1;
 
 	return clk_div;
+}
+
+static unsigned int spi_engine_get_word_length(struct spi_engine *spi_engine,
+	struct spi_device *spi, struct spi_transfer *xfer)
+{
+	/* if bits_per_word is 0 this will default to 8 bit words */
+	if (xfer->bits_per_word)
+		return xfer->bits_per_word;
+	else if (spi->bits_per_word)
+		return spi->bits_per_word;
+	else
+		return 8;
+}
+
+static void spi_engine_update_xfer_len(struct spi_engine *spi_engine,
+				       struct spi_transfer *xfer)
+{
+	unsigned int word_length = spi_engine->word_length;
+	unsigned int word_len_bytes = word_length / 8;
+
+	if ((xfer->len * 8) < word_length)
+		xfer->len = 1;
+	else
+		xfer->len = DIV_ROUND_UP(xfer->len, word_len_bytes);
 }
 
 static void spi_engine_gen_xfer(struct spi_engine_program *p, bool dry,
@@ -163,8 +209,8 @@ static void spi_engine_gen_xfer(struct spi_engine_program *p, bool dry,
 }
 
 static void spi_engine_gen_sleep(struct spi_engine_program *p, bool dry,
-	struct spi_engine *spi_engine, unsigned int clk_div,
-	struct spi_transfer *xfer)
+	struct spi_engine *spi_engine, struct spi_delay spi_delay,
+	unsigned int clk_div, struct spi_transfer *xfer)
 {
 	unsigned int spi_clk = clk_get_rate(spi_engine->ref_clk);
 	unsigned int t;
@@ -179,6 +225,8 @@ static void spi_engine_gen_sleep(struct spi_engine_program *p, bool dry,
 		return;
 
 	t = DIV_ROUND_UP(delay * spi_clk, (clk_div + 1) * 2);
+	/* spi_clk is in Hz while delay is usec, a division is required */
+	t /= 1000000;
 	while (t) {
 		unsigned int n = min(t, 256U);
 
@@ -193,7 +241,7 @@ static void spi_engine_gen_cs(struct spi_engine_program *p, bool dry,
 	unsigned int mask = 0xff;
 
 	if (assert)
-		mask ^= BIT(spi_get_chipselect(spi, 0));
+		mask ^= BIT(spi->chip_select);
 
 	spi_engine_program_add_cmd(p, dry, SPI_ENGINE_CMD_ASSERT(1, mask));
 }
@@ -204,9 +252,11 @@ static int spi_engine_compile_message(struct spi_engine *spi_engine,
 	struct spi_device *spi = msg->spi;
 	struct spi_transfer *xfer;
 	int clk_div, new_clk_div;
+	int word_len, new_word_len;
 	bool cs_change = true;
 
 	clk_div = -1;
+	word_len = -1;
 
 	spi_engine_program_add_cmd(p, dry,
 		SPI_ENGINE_CMD_WRITE(SPI_ENGINE_CMD_REG_CONFIG,
@@ -221,11 +271,22 @@ static int spi_engine_compile_message(struct spi_engine *spi_engine,
 					clk_div));
 		}
 
+		new_word_len = spi_engine_get_word_length(spi_engine, spi, xfer);
+		if (new_word_len != word_len) {
+			word_len = new_word_len;
+			spi_engine->word_length = word_len;
+			spi_engine_program_add_cmd(p, dry,
+				SPI_ENGINE_CMD_WRITE(SPI_ENGINE_CMD_REG_WORD_LENGTH,
+					word_len));
+		}
+
 		if (cs_change)
 			spi_engine_gen_cs(p, dry, spi, true);
 
+		spi_engine_update_xfer_len(spi_engine, xfer);
 		spi_engine_gen_xfer(p, dry, xfer);
-		spi_engine_gen_sleep(p, dry, spi_engine, clk_div, xfer);
+		spi_engine_gen_sleep(p, dry, spi_engine, xfer->delay, clk_div,
+				     xfer);
 
 		cs_change = xfer->cs_change;
 		if (list_is_last(&xfer->transfer_list, &msg->transfers))
@@ -233,10 +294,89 @@ static int spi_engine_compile_message(struct spi_engine *spi_engine,
 
 		if (cs_change)
 			spi_engine_gen_cs(p, dry, spi, false);
+
+		if (xfer->word_delay.value)
+			spi_engine_gen_sleep(p, dry, spi_engine,
+					     xfer->word_delay, clk_div, xfer);
 	}
 
 	return 0;
 }
+
+bool spi_engine_offload_supported(struct spi_device *spi)
+{
+	if (strcmp(spi->master->dev.parent->driver->name, "spi-engine") != 0)
+		return false;
+
+	return true;
+}
+EXPORT_SYMBOL_GPL(spi_engine_offload_supported);
+
+void spi_engine_offload_enable(struct spi_device *spi, bool enable)
+{
+	struct spi_master *master = spi->master;
+	struct spi_engine *spi_engine = spi_master_get_devdata(master);
+	unsigned int reg;
+
+	reg = readl(spi_engine->base + SPI_ENGINE_REG_OFFLOAD_CTRL(0));
+	if (enable)
+		reg |= SPI_ENGINE_OFFLOAD_CTRL_ENABLE;
+	else
+		reg &= ~SPI_ENGINE_OFFLOAD_CTRL_ENABLE;
+	writel(reg, spi_engine->base + SPI_ENGINE_REG_OFFLOAD_CTRL(0));
+}
+EXPORT_SYMBOL_GPL(spi_engine_offload_enable);
+
+int spi_engine_offload_load_msg(struct spi_device *spi,
+	struct spi_message *msg)
+{
+	struct spi_master *master = spi->master;
+	struct spi_engine *spi_engine = spi_master_get_devdata(master);
+	struct spi_engine_program p_dry;
+	struct spi_engine_program *p;
+	struct spi_transfer *xfer;
+	void __iomem *cmd_addr;
+	void __iomem *sdo_addr;
+	const uint32_t *buf;
+	unsigned int i, j;
+	size_t size;
+
+	msg->spi = spi;
+
+	p_dry.length = 0;
+	spi_engine_compile_message(spi_engine, msg, true, &p_dry);
+
+	size = sizeof(*p->instructions) * (p_dry.length + 2);
+
+	p = kzalloc(sizeof(*p) + size, GFP_KERNEL);
+	if (!p)
+		return -ENOMEM;
+
+	cmd_addr = spi_engine->base + SPI_ENGINE_REG_OFFLOAD_CMD_MEM(0);
+	sdo_addr = spi_engine->base + SPI_ENGINE_REG_OFFLOAD_SDO_MEM(0);
+
+	spi_engine_compile_message(spi_engine, msg, false, p);
+	spi_engine_program_add_cmd(p, false, SPI_ENGINE_CMD_SYNC(0));
+
+	writel(1, spi_engine->base + SPI_ENGINE_REG_OFFLOAD_RESET(0));
+	writel(0, spi_engine->base + SPI_ENGINE_REG_OFFLOAD_RESET(0));
+	j = 0;
+	list_for_each_entry(xfer, &msg->transfers, transfer_list) {
+		if (!xfer->tx_buf)
+			continue;
+		buf = xfer->tx_buf;
+		for (i = 0; i < xfer->len; i++, j++)
+			writel(buf[i], sdo_addr);
+	}
+
+	for (i = 0; i < p->length; i++)
+		writel(p->instructions[i], cmd_addr);
+
+	kfree(p);
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(spi_engine_offload_load_msg);
 
 static void spi_engine_xfer_next(struct spi_engine *spi_engine,
 	struct spi_transfer **_xfer)
@@ -310,19 +450,52 @@ static bool spi_engine_write_cmd_fifo(struct spi_engine *spi_engine)
 	return spi_engine->cmd_length != 0;
 }
 
-static bool spi_engine_write_tx_fifo(struct spi_engine *spi_engine)
+static void spi_engine_read_buff(struct spi_engine *spi_engine, uint8_t m)
+{
+	void __iomem *addr = spi_engine->base + SPI_ENGINE_REG_SDI_DATA_FIFO;
+	uint32_t val;
+	uint8_t bytes_len, i, j, *buf;
+
+	bytes_len = DIV_ROUND_UP(spi_engine->word_length, 8);
+	buf = spi_engine->rx_buf;
+
+	for (i = 0; i < (m * bytes_len); i += bytes_len) {
+		val = readl_relaxed(addr);
+		for (j = 0; j < bytes_len; j++)
+			buf[j] = val >> (8 * j);
+		buf += bytes_len;
+	}
+
+	spi_engine->rx_buf += i;
+}
+
+static void spi_engine_write_buff(struct spi_engine *spi_engine, uint8_t m)
 {
 	void __iomem *addr = spi_engine->base + SPI_ENGINE_REG_SDO_DATA_FIFO;
-	unsigned int n, m, i;
-	const uint8_t *buf;
+	uint8_t bytes_len, word_len, i, j;
+	uint32_t val = 0;
+
+	bytes_len = DIV_ROUND_DOWN_ULL(spi_engine->word_length, 8);
+	word_len = spi_engine->word_length;
+
+	for (i = 0; i < (m * bytes_len); i += bytes_len) {
+		for (j = 0; j < bytes_len; j++)
+			val |= spi_engine->tx_buf[i + j] << (word_len - 8 * (j + 1));
+		writel_relaxed(val, addr);
+		val = 0;
+	}
+
+	spi_engine->tx_buf += i;
+}
+
+static bool spi_engine_write_tx_fifo(struct spi_engine *spi_engine)
+{
+	unsigned int n, m;
 
 	n = readl_relaxed(spi_engine->base + SPI_ENGINE_REG_SDO_FIFO_ROOM);
 	while (n && spi_engine->tx_length) {
 		m = min(n, spi_engine->tx_length);
-		buf = spi_engine->tx_buf;
-		for (i = 0; i < m; i++)
-			writel_relaxed(buf[i], addr);
-		spi_engine->tx_buf += m;
+		spi_engine_write_buff(spi_engine, m);
 		spi_engine->tx_length -= m;
 		n -= m;
 		if (spi_engine->tx_length == 0)
@@ -334,17 +507,12 @@ static bool spi_engine_write_tx_fifo(struct spi_engine *spi_engine)
 
 static bool spi_engine_read_rx_fifo(struct spi_engine *spi_engine)
 {
-	void __iomem *addr = spi_engine->base + SPI_ENGINE_REG_SDI_DATA_FIFO;
-	unsigned int n, m, i;
-	uint8_t *buf;
+	unsigned int n, m;
 
 	n = readl_relaxed(spi_engine->base + SPI_ENGINE_REG_SDI_FIFO_LEVEL);
 	while (n && spi_engine->rx_length) {
 		m = min(n, spi_engine->rx_length);
-		buf = spi_engine->rx_buf;
-		for (i = 0; i < m; i++)
-			buf[i] = readl_relaxed(addr);
-		spi_engine->rx_buf += m;
+		spi_engine_read_buff(spi_engine, m);
 		spi_engine->rx_length -= m;
 		n -= m;
 		if (spi_engine->rx_length == 0)
@@ -354,15 +522,26 @@ static bool spi_engine_read_rx_fifo(struct spi_engine *spi_engine)
 	return spi_engine->rx_length != 0;
 }
 
+static void spi_engine_complete_message(struct spi_master *master, int status)
+{
+	struct spi_engine *spi_engine = spi_master_get_devdata(master);
+	struct spi_message *msg = spi_engine->msg;
+
+	kfree(spi_engine->p);
+	msg->status = status;
+	msg->actual_length = msg->frame_length;
+	spi_engine->msg = NULL;
+	spi_finalize_current_message(master);
+}
+
 static irqreturn_t spi_engine_irq(int irq, void *devid)
 {
-	struct spi_controller *host = devid;
-	struct spi_engine *spi_engine = spi_controller_get_devdata(host);
+	struct spi_master *master = devid;
+	struct spi_engine *spi_engine = spi_master_get_devdata(master);
 	unsigned int disable_int = 0;
 	unsigned int pending;
 
 	pending = readl_relaxed(spi_engine->base + SPI_ENGINE_REG_INT_PENDING);
-
 	if (pending & SPI_ENGINE_INT_SYNC) {
 		writel_relaxed(SPI_ENGINE_INT_SYNC,
 			spi_engine->base + SPI_ENGINE_REG_INT_PENDING);
@@ -390,13 +569,11 @@ static irqreturn_t spi_engine_irq(int irq, void *devid)
 	if (pending & SPI_ENGINE_INT_SYNC) {
 		if (spi_engine->msg &&
 		    spi_engine->completed_id == spi_engine->sync_id) {
-			struct spi_message *msg = spi_engine->msg;
 
-			kfree(spi_engine->p);
-			msg->status = 0;
-			msg->actual_length = msg->frame_length;
-			spi_engine->msg = NULL;
-			spi_finalize_current_message(host);
+			del_timer(&spi_engine->watchdog_timer);
+
+			spi_engine_complete_message(master, 0);
+
 			disable_int |= SPI_ENGINE_INT_SYNC;
 		}
 	}
@@ -412,14 +589,29 @@ static irqreturn_t spi_engine_irq(int irq, void *devid)
 	return IRQ_HANDLED;
 }
 
-static int spi_engine_transfer_one_message(struct spi_controller *host,
+static void spi_engine_timeout(struct timer_list *t)
+{
+	struct spi_engine *spi_engine = from_timer(spi_engine, t, watchdog_timer);
+	struct spi_master *master = spi_engine->master;
+
+	spin_lock(&spi_engine->lock);
+	if (spi_engine->msg) {
+		dev_err(&master->dev, "Timeout occured while waiting for transfer to complete. Hardware is probably broken.\n");
+		spi_engine_complete_message(master, -ETIMEDOUT);
+	}
+	spin_unlock(&spi_engine->lock);
+}
+
+static int spi_engine_transfer_one_message(struct spi_master *master,
 	struct spi_message *msg)
 {
 	struct spi_engine_program p_dry, *p;
-	struct spi_engine *spi_engine = spi_controller_get_devdata(host);
+	struct spi_engine *spi_engine = spi_master_get_devdata(master);
 	unsigned int int_enable = 0;
 	unsigned long flags;
 	size_t size;
+
+	del_timer_sync(&spi_engine->watchdog_timer);
 
 	p_dry.length = 0;
 	spi_engine_compile_message(spi_engine, msg, true, &p_dry);
@@ -458,48 +650,51 @@ static int spi_engine_transfer_one_message(struct spi_controller *host,
 	spi_engine->int_enable = int_enable;
 	spin_unlock_irqrestore(&spi_engine->lock, flags);
 
+	mod_timer(&spi_engine->watchdog_timer, jiffies + 5*HZ);
+
 	return 0;
 }
 
 static int spi_engine_probe(struct platform_device *pdev)
 {
 	struct spi_engine *spi_engine;
-	struct spi_controller *host;
+	struct spi_master *master;
 	unsigned int version;
 	int irq;
 	int ret;
 
 	irq = platform_get_irq(pdev, 0);
-	if (irq < 0)
-		return irq;
+	if (irq <= 0)
+		return -ENXIO;
 
 	spi_engine = devm_kzalloc(&pdev->dev, sizeof(*spi_engine), GFP_KERNEL);
 	if (!spi_engine)
 		return -ENOMEM;
 
-	host = spi_alloc_host(&pdev->dev, 0);
-	if (!host)
+	master = spi_alloc_master(&pdev->dev, 0);
+	if (!master)
 		return -ENOMEM;
 
-	spi_controller_set_devdata(host, spi_engine);
+	spi_master_set_devdata(master, spi_engine);
+	spi_engine->master = master;
 
 	spin_lock_init(&spi_engine->lock);
 
 	spi_engine->clk = devm_clk_get(&pdev->dev, "s_axi_aclk");
 	if (IS_ERR(spi_engine->clk)) {
 		ret = PTR_ERR(spi_engine->clk);
-		goto err_put_host;
+		goto err_put_master;
 	}
 
 	spi_engine->ref_clk = devm_clk_get(&pdev->dev, "spi_clk");
 	if (IS_ERR(spi_engine->ref_clk)) {
 		ret = PTR_ERR(spi_engine->ref_clk);
-		goto err_put_host;
+		goto err_put_master;
 	}
 
 	ret = clk_prepare_enable(spi_engine->clk);
 	if (ret)
-		goto err_put_host;
+		goto err_put_master;
 
 	ret = clk_prepare_enable(spi_engine->ref_clk);
 	if (ret)
@@ -525,46 +720,48 @@ static int spi_engine_probe(struct platform_device *pdev)
 	writel_relaxed(0xff, spi_engine->base + SPI_ENGINE_REG_INT_PENDING);
 	writel_relaxed(0x00, spi_engine->base + SPI_ENGINE_REG_INT_ENABLE);
 
-	ret = request_irq(irq, spi_engine_irq, 0, pdev->name, host);
+	ret = request_irq(irq, spi_engine_irq, 0, pdev->name, master);
 	if (ret)
 		goto err_ref_clk_disable;
 
-	host->dev.of_node = pdev->dev.of_node;
-	host->mode_bits = SPI_CPOL | SPI_CPHA | SPI_3WIRE;
-	host->bits_per_word_mask = SPI_BPW_MASK(8);
-	host->max_speed_hz = clk_get_rate(spi_engine->ref_clk) / 2;
-	host->transfer_one_message = spi_engine_transfer_one_message;
-	host->num_chipselect = 8;
+	master->dev.of_node = pdev->dev.of_node;
+	master->mode_bits = SPI_CPOL | SPI_CPHA | SPI_3WIRE;
+	master->max_speed_hz = clk_get_rate(spi_engine->ref_clk) / 2;
+	master->bits_per_word_mask = GENMASK(31, 0);
+	master->transfer_one_message = spi_engine_transfer_one_message;
+	master->num_chipselect = 8;
 
-	ret = spi_register_controller(host);
+	timer_setup(&spi_engine->watchdog_timer, spi_engine_timeout, 0);
+
+	ret = spi_register_master(master);
 	if (ret)
 		goto err_free_irq;
 
-	platform_set_drvdata(pdev, host);
+	platform_set_drvdata(pdev, master);
 
 	return 0;
 err_free_irq:
-	free_irq(irq, host);
+	free_irq(irq, master);
 err_ref_clk_disable:
 	clk_disable_unprepare(spi_engine->ref_clk);
 err_clk_disable:
 	clk_disable_unprepare(spi_engine->clk);
-err_put_host:
-	spi_controller_put(host);
+err_put_master:
+	spi_master_put(master);
 	return ret;
 }
 
-static void spi_engine_remove(struct platform_device *pdev)
+static int spi_engine_remove(struct platform_device *pdev)
 {
-	struct spi_controller *host = spi_controller_get(platform_get_drvdata(pdev));
-	struct spi_engine *spi_engine = spi_controller_get_devdata(host);
+	struct spi_master *master = spi_master_get(platform_get_drvdata(pdev));
+	struct spi_engine *spi_engine = spi_master_get_devdata(master);
 	int irq = platform_get_irq(pdev, 0);
 
-	spi_unregister_controller(host);
+	spi_unregister_master(master);
 
-	free_irq(irq, host);
+	free_irq(irq, master);
 
-	spi_controller_put(host);
+	spi_master_put(master);
 
 	writel_relaxed(0xff, spi_engine->base + SPI_ENGINE_REG_INT_PENDING);
 	writel_relaxed(0x00, spi_engine->base + SPI_ENGINE_REG_INT_ENABLE);
@@ -572,6 +769,8 @@ static void spi_engine_remove(struct platform_device *pdev)
 
 	clk_disable_unprepare(spi_engine->ref_clk);
 	clk_disable_unprepare(spi_engine->clk);
+
+	return 0;
 }
 
 static const struct of_device_id spi_engine_match_table[] = {
@@ -582,7 +781,7 @@ MODULE_DEVICE_TABLE(of, spi_engine_match_table);
 
 static struct platform_driver spi_engine_driver = {
 	.probe = spi_engine_probe,
-	.remove_new = spi_engine_remove,
+	.remove = spi_engine_remove,
 	.driver = {
 		.name = "spi-engine",
 		.of_match_table = spi_engine_match_table,
